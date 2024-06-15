0539a010a2f887e98f1d44c305989ce1f7bb2309
marlontorres17 <marlontorresmedina@gmail.com>
2024-06-15 13:36:57 -0500

    V 1.0  creacion maven

---
+++ BACK/metm_test_ws/src/main/java/com/sena/metm_test_ws/MetmTestWsApplication.java
@@ -0,0 +1,13 @@
+package com.sena.metm_test_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetmTestWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetmTestWsApplication.class, args);
+	}
+
+}
